635654fa306d3d9ac74fbc6f6f3cf2c6d349078e
jameswmr <jamescherry9236@gmail.com>
2023-04-16 19:32:22 +0800

    test

--- frontend/src/pages/CommentList.jsx
+++
@@ -1,9 +0,0 @@
-import { Comment } from "./comment"
-
-export function CommentList({ comments }) {
-  return comments.map(comment => (
-    <div key={comment.id} className="comment-stack">
-      <Comment {...comment} />
-    </div>
-  ))
-}
